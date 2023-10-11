8f64998c3fde4b659ec2dd56daef00dfacb8ccae
Sayantika Dhar <sayantika.dhar04@gmail.com>
2023-10-11 15:50:33 +0530

    add state and city id  data 11.10.2023

--- routes/PartnerRouter.js
+++ routes/PartnerRouter.js
@@ -109,7 +109,7 @@ PartnerRouter.get("/partner_match", async (req, res) => {
       (pref_dt.msg[0].own_gender != 'M' ? 
       `AND DATE_FORMAT(from_days(datediff(now(), a.dob)), '%Y')+0 >= DATE_FORMAT(from_days(datediff(now(), '${dateFormat(pref_dt.msg[0].dob, 'yyyy-mm-dd HH:MM:ss')}')), '%Y')+0` : 
       `AND DATE_FORMAT(from_days(datediff(now(), a.dob)), '%Y')+0 <= DATE_FORMAT(from_days(datediff(now(), '${dateFormat(pref_dt.msg[0].dob, 'yyyy-mm-dd HH:MM:ss')}')), '%Y')+0`) : 
-    '')}`;
+    '')} LIMIT ${data.min >= 0 ? data.min : 0}${data.max >= 0 ? ','+data.max : ''}`;
     var res_dt = await db_Select(select, table_name, whr, order);
     // console.log('PDt', res_dt);
 
