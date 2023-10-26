81f0d81a537a9572e52b8a4d07d37ab038097c9c
Sayantika Dhar <sayantika.dhar04@gmail.com>
2023-10-26 16:03:46 +0530

    testing 26.10.2023

--- routes/ProfileRouter.js
+++ routes/ProfileRouter.js
@@ -99,9 +99,9 @@ ProfileRouter.post("/user_groom_loc", async (req, res) => {
   var table_name = "td_user_profile",
     fields =
       chk_dt.suc > 0 && chk_dt.msg.length > 0
-        ? `country_id = '${data.field_Country}', state_id = '${data.field_State}', city_id = '${data.field_City > 0 ? data.field_City : 0}', modified_by = '${data.user}', modified_dt = '${datetime}'`
+        ? `country_id = '${data.field_Country}', state_id = '${data.field_State > 0 ? data.field_State : 0}', city_id = '${data.field_City > 0 ? data.field_City : 0}', modified_by = '${data.user}', modified_dt = '${datetime}'`
         : "(country_id, state_id, city_id, created_by, created_dt)",
-    values = `('${data.field_Country}', '${data.field_State}', '${data.field_City > 0 ? data.field_City : 0}', '${data.user}', '${datetime}')`,
+    values = `('${data.field_Country}', '${data.field_State > 0 ? data.field_State : 0}', '${data.field_City > 0 ? data.field_City : 0}', '${data.user}', '${datetime}')`,
     whr =
       chk_dt.suc > 0 && chk_dt.msg.length > 0
         ? `id = ${chk_dt.msg[0].id}`
