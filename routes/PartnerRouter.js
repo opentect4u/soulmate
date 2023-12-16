4fe0d32d29f58f5cd468dbe9f5619e0dbd6bc07f
Tan <mondal.tanmoy@synergicsoftek.com>
2023-12-16 18:33:44 +0530

    Update active flag 16-12-2023

--- routes/PartnerRouter.js
+++ routes/PartnerRouter.js
@@ -350,11 +350,11 @@ PartnerRouter.get("/partner_match", async (req, res) => {
   // console.log('Pref', pref_dt);
 
   if(pref_dt.suc > 0 && pref_dt.msg.length > 0){
-    var select = "a.id, a.dob, a.gender,c.last_login,a.pay_flag",
+    var select = "a.id, a.dob, a.gender,c.last_login,a.pay_flag, a.active_flag",
     table_name = "td_user_profile a LEFT JOIN td_user_p_dtls b ON a.id=b.user_id LEFT JOIN md_user_login c ON a.id = c.profile_id",
-    whr = `a.kundali_file_name IS NOT NULL AND a.active_flag = 'Y'`
+    whr = `a.kundali_file_name IS NOT NULL`
     order = `GROUP BY a.id
-    HAVING a.gender != '${pref_dt.msg[0].gender}' 
+    HAVING a.active_flag = 'Y' AND a.gender != '${pref_dt.msg[0].gender}' 
     ${pref_dt.msg[0].age_frm > 0 || pref_dt.msg[0].age_to > 0 ?
     (`${pref_dt.msg[0].age_frm > 0 ? `AND DATE_FORMAT(from_days(datediff(now(), a.dob)), '%Y')+0 >= ${pref_dt.msg[0].age_frm}` : ''} 
       ${pref_dt.msg[0].age_to > 0 ? `AND DATE_FORMAT(from_days(datediff(now(), dob)), '%Y')+0 <= ${pref_dt.msg[0].age_to}` : ''}`
